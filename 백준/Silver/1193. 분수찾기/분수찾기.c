f9e87f990367ba3cdd3e2a9aadc3fbf1e617b9c7
MIDORI <sohyun22@ajou.ac.kr>
2024-01-20 20:37:32 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.c
+++ 백준/Silver/1193. 분수찾기/분수찾기.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
-int count=0;
-void FindLine(int n){
-    if(n<=0) return;
 
-    count++;
-    n=n-count;
-    FindLine(n);
-}
 int main() {
     int x;
     scanf("%d",&x);
-    FindLine(x);
+    int count=0,sum=0;
 
-    int sum=0;
-    for(int i=1;i<=count;i++){
-        sum+=i;
+    while(1) {
+        sum += count;
+        if(sum>=x) break;
+        count++;
     }
 
     if(count%2==0)
         printf("%d/%d",count-(sum-x),1+(sum-x));
     else
         printf("%d/%d",1+(sum-x),count-(sum-x));
-    
+
 }
