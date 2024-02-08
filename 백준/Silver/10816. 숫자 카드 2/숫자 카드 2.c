44086414d56071af244c8a5aa855d7b5b9b69577
MIDORI <sohyun22@ajou.ac.kr>
2024-02-08 17:40:41 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 332 ms, Memory: 84984 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.c
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-int compare(const void *a,const void *b){
-    return *(int *)a-*(int *)b;
-}
-
 int main() {
     int n;
 
     scanf("%d",&n);
 
-    int a[n];
+    int a[500000];
 
     for(int i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
 
-    qsort(a,n,sizeof(int),compare);
-
-
-    int *hash_p;
-    int *hash_n;
-    if(a[0]<0&&a[n-1]>0){
-        hash_p=(int*)calloc(a[n-1]+1,sizeof(int));
-        hash_n=(int*)calloc(-a[0]+1,sizeof(int));
-    }
-    else if(a[0]>=0)
-        hash_p=(int*)calloc((a[n-1]+1),sizeof(int));
-    else if(a[n-1]<0)
-        hash_n=(int*)calloc((-a[0]+1),sizeof(int));
-
-
+    int *hash_p=(int*)calloc(10000001,sizeof(int));
+    int *hash_n=(int*)calloc(10000001,sizeof(int));
 
     for(int i=0;i<n;i++){
         if(a[i]<0)
@@ -40,23 +23,23 @@ int main() {
 
     int m;
     scanf("%d",&m);
-    int b[m];
-    int k[m];
+    int b[500000];
+    int k[500000];
 
     for(int i=0;i<m;i++){
         scanf("%d",&b[i]);
-        if(b[i]<0&&b[i]>=a[0])
+        if(b[i]<0&&b[i]>=-10000000)
             k[i]=hash_n[-b[i]];
 
-        else if(b[i]>=0&&b[i]<=a[n-1])
+        else if(b[i]>=0&&b[i]<=10000000)
             k[i]=hash_p[b[i]];
-
-        else
-            k[i]=0;
 
     }
     for(int i=0;i<m;i++){
         printf("%d ",k[i]);
     }
-}
 
+
+    free(hash_p);
+    free(hash_n);
+}
