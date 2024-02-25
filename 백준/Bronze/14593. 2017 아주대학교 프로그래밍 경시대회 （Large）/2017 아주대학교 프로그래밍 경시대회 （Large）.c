ba82f861db8eac94f6322643c2d5a80f9895e8bb
MIDORI <sohyun22@ajou.ac.kr>
2024-02-26 03:51:34 +0900

    [Bronze II] Title: 2017 아주대학교 프로그래밍 경시대회 (Large), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14593. 2017 아주대학교 프로그래밍 경시대회 （Large）/2017 아주대학교 프로그래밍 경시대회 （Large）.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    int p[10000][3];
+    int maxscore=-1;
+    int submit;
+    int lasttime;
+    int winner;
+
+    for(int i=0;i<n;i++){
+        scanf("%d %d %d",&p[i][0],&p[i][1],&p[i][2]);
+    }
+
+    for(int i=0;i<n;i++){
+        if(maxscore<p[i][0]){
+            maxscore=p[i][0];
+            submit=p[i][1];
+            lasttime=p[i][2];
+            winner=i;
+        }
+        else if(maxscore==p[i][0]){
+            if(submit>p[i][1]){
+                submit=p[i][1];
+                winner=i;
+            }
+            else if(submit==p[i][1]){
+                if(lasttime>p[i][2]){
+                    winner=i;
+                }
+            }
+        }
+    }
+    printf("%d",winner+1);
+
+}
