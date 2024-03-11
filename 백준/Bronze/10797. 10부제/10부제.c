223cd5988ce984e013d677b5a321a9f45fb2ac9d
MIDORI <sohyun22@ajou.ac.kr>
2024-03-11 13:32:40 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+    int car;
+    int count=0;
+
+    scanf("%d",&day);
+
+    for(int i=0;i<5;i++){
+        scanf("%d",&car);
+        if(day%10==car) count++;
+    }
+
+    printf("%d",count);
+}
