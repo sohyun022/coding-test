4243fecd6e43f65d598e8e2597410534d492a91e
소현 <sohyun22@ajou.ac.kr>
2025-02-01 05:22:47 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -8,19 +8,13 @@ using namespace std;
 int solution(vector<int> citations) {
     int hIndex = 0;
     
-    sort(citations.begin(),citations.end());
+    sort(citations.begin(),citations.end(),greater<int>());
 
     int n=citations.size();
-    
-    if(n<citations[0]){
-        return n;    
-    }
-    
+   
     for(int i=0;i<n;i++){
-        if(citations[i]<= n-i){
-            hIndex=citations[i];
-            while(hIndex+1 <= n-i-1 )
-                hIndex++;
+        if(citations[i] >= i+1){
+            hIndex=i+1;
         }
     }
     
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.04 ms
+메모리: 4.14 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 10:28:29
+2025년 02월 01일 05:22:39
 
 ### 문제 설명
 
